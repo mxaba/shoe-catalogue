e7c3152030f5e4249bac252714468653e94ec1c0
mxaba <samuel.mcebo@gmail.com>
2021-05-14 12:37:34 +0200

    I can search and pass the data to the template

--- js/shoe-catalogue-dom.js
+++ js/shoe-catalogue-dom.js
@@ -5,12 +5,54 @@ document.addEventListener('DOMContentLoaded', function(){
     var shoeSection = document.querySelector('.shoe-section')
 
     var initLogic = ShoesCatalogue()
+    var shoeLocal = {}
 
-    displayShoes()
+    if (localStorage['shoeCatalogue']){
+        console.log('Local Data')
+        shoeLocal = JSON.parse(localStorage.getItem('shoeCatalogue'))
+    } else {
+        console.log('Own Data')
+        shoeLocal = Data()
+    }
 
-    function displayShoes(){
-        shoeSection.innerHTML = shoeCompiledTemplate({shoesList: initLogic.getShoeData()} )
+    initLogic.setShoeData(shoeLocal)
+    displayShoes(shoeLocal)
+
+    function displayShoes(array){
+        shoeSection.innerHTML = shoeCompiledTemplate({shoesList: array} )
         // console.log(shoeCompiledTemplate({shoesList: initLogic.getShoeData()} ))
     }
+
+    document.querySelector('.search').addEventListener('click', function(){
+        var colorValue = document.getElementById('color').value
+        var brandValue = document.getElementById('brand').value
+        var sizeValue = document.getElementById('size').value
+
+        // console.log(brandValue)
+        // console.log(colorValue)
+        // console.log(sizeValue)
+
+        var searchData = {}
+        
+        if (colorValue !== ""){
+            searchData.color = colorValue
+        } if (sizeValue !== ""){
+            searchData.size = sizeValue
+        } if (brandValue !== "") {
+            searchData.brand = brandValue
+        }
+        // console.log(searchData)
+        // shoeSection.innerHTML = ""
+        var lenFilter = initLogic.shoesFilterFunction(searchData)
+        if (lenFilter.length != 0){
+            shoeSection.innerHTML = shoeCompiledTemplate({shoesList: initLogic.shoesFilterFunction(searchData)})
+            
+        } else {
+            shoeSection.innerHTML = "No Results found!🤦🏾‍♂️"
+            setTimeout(function(){
+                displayShoes(shoeLocal)
+            }, 3000)
+        }
+    })
 
 })
--- js/shoe-catalogue-logic.js
+++ js/shoe-catalogue-logic.js
@@ -1,15 +1,35 @@
 function ShoesCatalogue(){
 
-    var shoeData = Data()
+    var shoeData = {}
+
+    function setShoeData(namePassed){
+        shoeData = namePassed
+    }
 
     function getShoeData(){
         return shoeData
     }
 
+    function shoesFilterFunction(shoesFlit){
+        var shoesFiltered = shoeData.filter(function(shoeData){
+            var shoesFlitObjKeys = Object.keys(shoesFlit)
+            var len = 0
+            for(var i=0; i < shoesFlitObjKeys.length; i++){
+                len += (shoeData[shoesFlitObjKeys[i]]  == shoesFlit[shoesFlitObjKeys[i]])
+            } 
+            if (len == shoesFlitObjKeys.length){
+                return true
+            }
+        })
+        // console.log(shoesFiltered)
+        return shoesFiltered
+    }
     // function
 
 
     return {
+        shoesFilterFunction,
+        setShoeData,
         getShoeData
     }
 }
