da342a9932675ecdcf66550bbd409d104aa0f2d8
mxaba <samuel.mcebo@gmail.com>
2021-05-19 11:21:45 +0200

    Added font awesome

--- js/shoe-catalogue-logic.js
+++ js/shoe-catalogue-logic.js
@@ -82,7 +82,7 @@ function ShoesCatalogue(){
             if (shoeAdded.in_stock > 0){
                 shoeAdded.in_stock--
                 // shoeAdded.qty++
-                if (shoeAdded.qty > 0){
+                if (cartStorage.includes(shoeAdded._id)){
                     cartStorage = cartStorage.filter(function(shoe){
                             // shoe.price = shoe.price * shoe.qty
                             shoe.qty++
